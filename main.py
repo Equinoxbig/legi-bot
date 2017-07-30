4a63841134bf8308a07168e38b4553cd8455b765
Equinoxbig <equinoxdxdxd@gmail.com>
2017-07-31 00:32:37 +0200

    Splitting in multiple files

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Code found here : https://gist.github.com/fourtonfish/5ac885e5e13e6ca33dca9f8c2ef1c46e
-# Original: https://paste.wuffs.org/raw/160201.220633.4garttu6
+# Code inspired and based on @fourtonfish's script
+# Code can be found here : https://gist.github.com/fourtonfish/5ac885e5e13e6ca33dca9f8c2ef1c46e
 
+# Importing libraries
 from twitter import *
 import json
-import os
+import pollster
 
 # Guide on how to get your tokens
 # The IPHONE key and secret can be found here : https://gist.github.com/shobotch/5160017
@@ -16,8 +17,8 @@ import os
 # Then store them in a .json file named credentials.json
 
 # Loads the credentials from a json file in order to use them in Oauth
-with open('credentials.json') as config_creditentials:
-    CREDENTIALS = json.load(config_creditentials)
+with open('credentials.json') as config_credentials:
+    CREDENTIALS = json.load(config_credentials)
 
     ACCESS_TOKEN = CREDENTIALS['ACCESS_TOKEN']
     ACCESS_TOKEN_SECRET = CREDENTIALS['ACCESS_TOKEN_SECRET']
@@ -25,7 +26,7 @@ with open('credentials.json') as config_creditentials:
     IPHONE_CONSUMER_SECRET = CREDENTIALS['IPHONE_CONSUMER_SECRET']
 
 
-# Spoofing the requests
+# Spoofing requests to twitter
 class SpoofOAuth(OAuth):
     def __init__(self, *args, **kwargs):
         OAuth.__init__(self, *args, **kwargs)
@@ -43,38 +44,18 @@ class SpoofOAuth(OAuth):
         return hdr
 
 
+# Generating "fake" Oauth
 auth = SpoofOAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
                   IPHONE_CONSUMER_KEY, IPHONE_CONSUMER_SECRET)
 
+
+# Connecting to APIs
 api = Twitter(auth=auth)
 caps_api = Twitter(domain='caps.twitter.com', api_version='v2', auth=auth)
 
 
-def tweet_poll(text, choice1, choice2):
-    # Tweet parameters
-    poll_text = text
-    poll_card = {
-        'twitter:string:choice1_label': choice1,
-        'twitter:string:choice2_label': choice2,
-        'twitter:long:duration_minutes': 1440,
-        'twitter:api:api:endpoint': '1',
-        'twitter:card': 'poll2choice_text_only',
-    }
-
-    # Generating the poll card from the twitter API before posting it as
-    # a tweet parameter
-    # https://caps.twitter.com/v2/cards/create.json?card_data=...&send_error_codes=1
-    card_data = caps_api.cards.create(
-        card_data=json.dumps(poll_card),
-        send_error_codes=1)
-
-    # Sending the tweet to twitter using the card generated above
-    # https://api.twitter.com/1.1/statuses/update.json?status=...&card_uri=...&cards_platform=iPhone-13&include_cards=1
-    api.statuses.update(
-        status=poll_text,
-        card_uri=card_data['card_uri'],
-        cards_platform='iPhone-13',
-        include_cards=1)
-
-
-tweet_poll(text="Ceci est un test", choice1="Test1", choice2="test2")
+# pollster.tweet_poll(api=api,
+#                    caps_api=caps_api,
+#                    text="Test12",
+#                    choice1="Test1",
+#                    choice2="Test2")
---
+++ pollster.py
@@ -0,0 +1,31 @@
+# still needing json lib
+import json
+
+
+# Function used to tweet a poll taking an twitter API connection and
+# a cards API access
+def tweet_poll(api, caps_api, text, choice1, choice2):
+    # Tweet parameters
+    poll_text = text
+    poll_card = {
+        'twitter:string:choice1_label': choice1,
+        'twitter:string:choice2_label': choice2,
+        'twitter:long:duration_minutes': 1440,
+        'twitter:api:api:endpoint': '1',
+        'twitter:card': 'poll2choice_text_only',
+    }
+
+    # Generating the poll card from the twitter API before posting it as
+    # a tweet parameter
+    # https://caps.twitter.com/v2/cards/create.json?card_data=...&send_error_codes=1
+    card_data = caps_api.cards.create(
+        card_data=json.dumps(poll_card),
+        send_error_codes=1)
+
+    # Sending the tweet to twitter using the card generated above
+    # https://api.twitter.com/1.1/statuses/update.json?status=...&card_uri=...&cards_platform=iPhone-13&include_cards=1
+    api.statuses.update(
+        status=poll_text,
+        card_uri=card_data['card_uri'],
+        cards_platform='iPhone-13',
+        include_cards=1)
